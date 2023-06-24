2a7c29ee269e465a983cfc1a033089b0b96e4fe3
The-Whole-Shell <michelmoghaudo@gmail.com>
2023-06-24 09:28:38 +0200

    creation of POO class

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,34 +1,36 @@
 // eslint-disable-next-line no-unused-vars
-function photographerFactory(data) {
-	const { name, portrait, city, country, tagline, price, id } = data;
-	const picture = `assets/photographers/${portrait}`;
-    
-	function getUserCardDOM() {
-		const article = document.createElement( "article" );
-		const img = document.createElement( "img" );
-		const a = document.createElement( "a" );
-		a.setAttribute("href", `/photographer.html?id=${id}`);
-		img.setAttribute("src", picture);
-		img.setAttribute("alt", name);
-		const h2 = document.createElement( "h2" );
-		h2.textContent = name;
-		const location = document.createElement( "p" );
-		location.textContent = `${city}, ${country}`;
-		const tagLine = document.createElement( "span" );
-		tagLine.classList.add("tagLine");
-		tagLine.textContent = tagline;
-		const prices = document.createElement( "span" );
-		prices.textContent = `${price}€/jour`;
-		prices.classList.add("prices");
-            
-		a.appendChild(img);
-		a.appendChild(h2);
-		article.appendChild(a);
-		article.appendChild(location);
-		article.appendChild(tagLine);
-		article.appendChild(prices);
-		return (article);
+class PhotographerFactory{
+	constructor(data) {
+		const { name, portrait, city, country, tagline, price, id } = data;
+		const picture = `assets/photographers/${portrait}`;
+		
+		function getUserCardDOM() {
+			const article = document.createElement( "article" );
+			const img = document.createElement( "img" );
+			const a = document.createElement( "a" );
+			a.setAttribute("href", `/photographer.html?id=${id}`);
+			img.setAttribute("src", picture);
+			img.setAttribute("alt", name);
+			const h2 = document.createElement( "h2" );
+			h2.textContent = name;
+			const location = document.createElement( "p" );
+			location.textContent = `${city}, ${country}`;
+			const tagLine = document.createElement( "span" );
+			tagLine.classList.add("tagLine");
+			tagLine.textContent = tagline;
+			const prices = document.createElement( "span" );
+			prices.textContent = `${price}€/jour`;
+			prices.classList.add("prices");
+				
+			a.appendChild(img);
+			a.appendChild(h2);
+			article.appendChild(a);
+			article.appendChild(location);
+			article.appendChild(tagLine);
+			article.appendChild(prices);
+			return (article);
+		}
+		
+		return { name, picture, getUserCardDOM };
 	}
-    
-	return { name, picture, getUserCardDOM };
 }
